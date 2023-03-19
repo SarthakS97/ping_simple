dc14d41b842b719f3fb04dc91be7fb52133e2bd5
SarthakS97 <sarthak1509@gmail.com>
2023-03-19 22:31:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+setInterval(() => {
+  const options = {
+    method: 'GET',
+    hostname: 'www.example.com',
+    port: 80,
+    path: '/',
+  };
+  
+  const req = http.request(options, (res) => {
+    console.log(`Website visited! Status code: ${res.statusCode}`);
+  });
+  
+  req.on('error', (e) => {
+    console.error(`Error: ${e.message}`);
+  });
+  
+  req.end();
+}, 900000); // 15 minutes in milliseconds
